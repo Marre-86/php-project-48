acef2d40fe0edcb23a4bb183eb37a94c088cd7d9
Artem Pokhiliuk <timtimoha@yandex.ru>
2022-11-15 18:11:28 +0600

    Fix for hexlet auto-check 6

--- src/Formatters/Plain.php
+++ src/Formatters/Plain.php
@@ -13,26 +13,35 @@ function iter($input, $path, $previous)
 {
     $result = array_map(function ($key, $value) use ($path, &$previous) {
         $string = null;
-        $realKey = substr($key, 2);              // отбрасываем служебные первые два символа
-        if (!is_array($value)) {
-            $realValue = (in_array($value, ['true', 'false', 'null'])) ? $value : "'{$value}'";
-        }
-        $currentValue = (is_array($value)) ? '[complex value]' : $realValue;
+        $currentKey = substr($key, 2);              // отбрасываем служебные первые два символа
+        $currentValue = normalizeValue($value);
         if ((Misc\isAssoc($value)) and ($key[0] === " ")) {
-            $path .= $realKey . ".";
-            $previous = (is_array($value)) ? [$realKey, '[complex value]'] : [$realKey, $realValue];
+            $path .= $currentKey . ".";
+            $previous = (is_array($value)) ? [$currentKey, '[complex value]'] : [$currentKey, $currentValue];
             $string = iter($value, $path, $previous);
-        } elseif ($previous[0] === $realKey) {
-             $string = "Property '{$path}{$realKey}' was updated. From {$previous[1]} to {$currentValue}";
+        } elseif ($previous[0] === $currentKey) {
+             $string = "Property '{$path}{$currentKey}' was updated. From {$previous[1]} to {$currentValue}";
         } elseif ($key[0] === "+") {
-            $string = "Property '{$path}{$realKey}' was added with value: {$currentValue}";
+            $string = "Property '{$path}{$currentKey}' was added with value: {$currentValue}";
         } elseif ($key[0] === "-") {
-            $string = "Property '{$path}{$realKey}' was removed";
+            $string = "Property '{$path}{$currentKey}' was removed";
         }
-        $previous = (is_array($value)) ? [$realKey, '[complex value]'] : [$realKey, $realValue]; // phpcs:ignore
+        $previous = (is_array($value)) ? [$currentKey, '[complex value]'] : [$currentKey, $currentValue]; // phpcs:ignore
         return $string;
     }, array_keys($input), $input);
     $result1 = Misc\removeRedundantItems(array_filter(Misc\flatten($result)));
     $string = implode("\n", $result1);
     return $string;
+}
+
+function normalizeValue($value)
+{
+    if (is_numeric($value)) {
+        return $value;
+    } elseif (!is_array($value)) {
+        $quotedValue = (in_array($value, ['true', 'false', 'null'])) ? $value : "'{$value}'";
+        return $quotedValue;
+    } else {
+        return '[complex value]';
+    }
 }
