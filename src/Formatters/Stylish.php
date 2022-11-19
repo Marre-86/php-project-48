270011b63ff91308e57b3faef988d476236874a3
Artem Pokhiliuk <timtimoha@yandex.ru>
2022-11-19 16:16:25 +0600

    Refactor src/Formatters/Stylish.php step9

--- src/Formatters/Stylish.php
+++ src/Formatters/Stylish.php
@@ -2,7 +2,7 @@
 
 namespace Gendiff\Formatters\Stylish;
 
-function stylish($input, $replacer = ' ', $margin = 2)
+function stylish(mixed $input, string $replacer = ' ', int $margin = 2)
 {
     if (!is_array($input)) {
         $result = (is_bool($input)) ? var_export($input, true) : $input;
@@ -13,22 +13,23 @@ function stylish($input, $replacer = ' ', $margin = 2)
     }
 }
 
-function iter(array $array, $prefix, $prefixCount)
+function iter(array $array, string $prefix, int $prefixCount)
 {
-    $strResult = "{\n";
+    $openingBracket = "{\n";
     $realPrefix = str_repeat($prefix, $prefixCount);
-    $bracketPrefix = str_repeat($prefix, $prefixCount - 2);
-    foreach ($array as $key => $value) {
+    $closingBracketPrefix = str_repeat($prefix, $prefixCount - 2);
+    $formattedArray = array_map(function ($key, $value) use ($realPrefix, $prefix, $prefixCount) {
         if (!is_array($value)) {
             $value = (is_bool($value)) ? var_export($value, true) : $value;
-            $strResult .= "{$realPrefix}{$key}: {$value}\n";
+            $formattedLine = "{$realPrefix}{$key}: {$value}\n";
         } else {
             $prefixCount += 4;
             $value = iter($value, $prefix, $prefixCount);
             $prefixCount -= 4;
-            $strResult .= "{$realPrefix}{$key}: {$value}";
+            $formattedLine = "{$realPrefix}{$key}: {$value}";
         }
-    }
-    $strResult .= $bracketPrefix . "}\n";
-    return $strResult;
+        return $formattedLine;
+    }, array_keys($array), $array);
+    $formattedString = $openingBracket . implode("", $formattedArray) . $closingBracketPrefix . "}\n";
+    return $formattedString;
 }
