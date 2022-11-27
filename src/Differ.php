17fa5964692f265a55a9e9d1c24c1d322575bcf9
Artem Pokhiliuk <timtimoha@yandex.ru>
2022-11-28 01:53:54 +0600

    Colossal refactor src/Differ.php step19

--- src/Differ.php
+++ src/Differ.php
@@ -10,8 +10,83 @@ function gendiff(string $pathToFile1, string $pathToFile2, string $format = 'sty
     $inputArr1 = Parsers\parseToArray($pathToFile1);
     $inputArr2 = Parsers\parseToArray($pathToFile2);
     $diff = buildDiff($inputArr1, $inputArr2);
-//    return $diff;
     return Formatters\format($diff, $format);
+}
+
+function buildDiff(array $array1, array $array2)
+{
+    $preResultKeys = array_map(function ($key, $value) use ($array1, $array2) {
+        if (array_key_exists($key, $array2)) {
+            if ((isAssoc($array1[$key])) && (isAssoc($array2[$key]))) {
+                return $key . '0 ';
+            } elseif (isAssoc($array1[$key])) {
+                return $key . "1-\n" . $key . '2+';
+            } elseif (isAssoc($array2[$key])) {
+                return $key . "1-\n" . $key . '2+';
+            } elseif (($value === $array2[$key]) or (is_array($value))) {
+                return $key . '0 ';
+            } else {
+                return $key . "1-\n" . $key . '2+';
+            }
+        } else {
+            return $key . '0-';
+        }
+    }, array_keys($array1), $array1);
+    $preResultValues = array_map(function ($key, $value) use ($array1, $array2) {
+        if (array_key_exists($key, $array2)) {
+            if ((isAssoc($array1[$key])) && (isAssoc($array2[$key]))) {
+                return buildDiff($array1[$key], $array2[$key]);
+            } elseif (isAssoc($array1[$key])) {
+                return [getChildren($array1[$key]), $array2[$key]];
+            } elseif (isAssoc($array2[$key])) {
+                return [$array1[$key], getChildren($array2[$key])];
+            } elseif (($value === $array2[$key]) or (is_array($value))) {
+                return $value;
+            } else {
+                return normValue($value) . "\n" . normValue($array2[$key]);
+            }
+        } else {
+            if (isAssoc($array1[$key])) {
+                return getChildren($array1[$key]);
+            } else {
+                return $value;
+            }
+        }
+    }, array_keys($array1), $array1);
+    $preResultPart1 = array_combine(mendArray($preResultKeys), mendArray($preResultValues));
+    $preResultPart2Keys = array_map(function ($key, $value) use ($array1, $array2) {
+        if (!array_key_exists($key, $array1)) {
+                return $key . '0+';
+        }
+    }, array_keys($array2), $array2);
+    $preResultPart2Values = array_map(function ($key, $value) use ($array1, $array2) {
+        if (!array_key_exists($key, $array1)) {
+            if (isAssoc($array2[$key])) {
+                return getChildren($array2[$key]);
+            } else {
+                return normValue($value);
+            }
+        }
+    }, array_keys($array2), $array2);
+    $preResultPart2 = array_filter(array_combine(mendArray($preResultPart2Keys), mendArray($preResultPart2Values)), fn($value) => !is_null($value)); // phpcs:ignore
+    $preResultNew = array_merge($preResultPart1, $preResultPart2);
+    ksort($preResultNew);
+    $resultNewKeys = array_map(function ($key, $value) {
+        $prefix = $key[-1];
+        $keyNew = $prefix . " " . substr($key, 0, -2);
+        return $keyNew;
+    }, array_keys($preResultNew), $preResultNew);
+    $newResult = array_combine($resultNewKeys, $preResultNew);
+    return $newResult;
+}
+
+function normValue(mixed $input)
+{
+    $value = ((is_bool($input)) or ($input === null)) ? var_export($input, true) : $input;
+    if ($value === "NULL") {
+        return "null";
+    }
+    return $value;
 }
 
 function isAssoc(mixed $arr)         // взял со Stackoverflow, чуток подправил
@@ -24,61 +99,29 @@ function isAssoc(mixed $arr)         // взял со Stackoverflow, чуток 
 
 function getChildren(array $array)         // весь смысл данной ф-ии - корректная индентация вложенных ассоц.массивов
 {
-    $newKeys = array_map(function ($k, $v) {
+    $newKeys = array_map(function ($k) {
             return '  ' . $k;
-    }, array_keys($array), $array);
+    }, array_keys($array));
     $newValues = array_map(function ($v) {
         return (isAssoc($v)) ? getChildren($v) : $v;
     }, $array);
     return array_combine($newKeys, $newValues);
 }
 
-function buildDiff(array $array1, array $array2)
+function mendArray(array $input)
 {
-    foreach ($array1 as $key => $value) {
-        if (array_key_exists($key, $array2)) {
-            if ((isAssoc($array1[$key])) && (isAssoc($array2[$key]))) {
-                $preResult[$key . '0 '] = buildDiff($array1[$key], $array2[$key]);
-            } elseif (isAssoc($array1[$key])) {
-                 $preResult[$key . '1-'] = getChildren($array1[$key]);
-                 $preResult[$key . '2+'] = $array2[$key];
-            } elseif (isAssoc($array2[$key])) {
-                 $preResult[$key . '1-'] = $array1[$key];
-                 $preResult[$key . '2+'] = getChildren($array2[$key]);
-            } elseif (($value === $array2[$key]) or (is_array($value))) {
-                $preResult[$key . '0 '] = $value;
-            } else {
-      // смысл первой цифры в постфиксе - корректная сортировка полей поменявших значение
-                $preResult[$key . '1-'] = $value;
-                $preResult[$key . '2+'] = $array2[$key];
-            }
+    $arrMended = array_reduce($input, function ($acc, $item) {
+        if (!is_array($item)) {
+            return array_merge($acc, (strstr($item, PHP_EOL) !== false) ? explode("\n", $item) : [$item]);
         } else {
-            if (isAssoc($array1[$key])) {
-                $preResult[$key . '0-'] = getChildren($array1[$key]);
+            if ((array_key_exists(0, $item)) and ((count($item) === 2) and isAssoc($item[0]) and (!is_array($item[1])))) {  // phpcs:ignore
+                return array_merge($acc, [$item[0]], [$item[1]]);
+            } elseif ((array_key_exists(0, $item)) and ((count($item) === 2) and isAssoc($item[1]) and (!is_array($item[0])))) { // phpcs:ignore
+                return array_merge($acc, [$item[0]], [$item[1]]);
             } else {
-                $preResult[$key . '0-'] = $value;
+                return array_merge($acc, [$item]);
             }
         }
-    }
-    foreach ($array2 as $key => $value) {
-        if (!array_key_exists($key, $array1)) {
-            if (isAssoc($array2[$key])) {
-                $preResult[$key . '0+'] = getChildren($array2[$key]);
-            } else {
-                $preResult[$key . '0+'] = $value;
-            }
-        }
-    }
-    ksort($preResult);
-    foreach ($preResult as $key => $value) {
-        $prefix = $key[-1];
-        $key = $prefix . " " . substr($key, 0, -2);
-        $value = ((is_bool($value)) or ($value === null)) ? var_export($value, true) : $value;
-        if ($value === "NULL") {
-            $value = "null";
-        }
-//        $value = var_export($value, true);
-        $result[$key] = $value;
-    }
-    return $result;
+    }, []);
+    return $arrMended;
 }
